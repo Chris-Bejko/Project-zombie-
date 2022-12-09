2fda173bba8b18e084c9263bba6146573b30cb09
Chris Bejko <92367999+Chris-Bejko@users.noreply.github.com>
2022-12-09 17:21:00 +0200

    Art import
    Credits to: https://waxx.itch.io/shadow-mines-asset-pack for the environment and https://secrethideout.itch.io/team-wars-platformer-battle for the main character

---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    public StateID StateID;
+}
+
+
+public enum StateID
+{
+
+}
